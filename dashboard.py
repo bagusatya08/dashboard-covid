b7402e235d3249dff6382043de5d8f3044aa889b
bagus-satya <bagusatya08@gmail.com>
2024-07-04 04:13:29 +0800

    updating the dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 import datetime
@@ -10,6 +11,7 @@ from IPython.display import display
 
 st.set_page_config(layout="wide")
 data_ch = pd.read_csv("dataset/updated_dataset_covid.csv")
+data_ch.dropna(inplace=True)
 
 # Konversi kolom "Date" menjadi datetime
 data_ch['Date'] = pd.to_datetime(data_ch['Date'])
@@ -17,7 +19,7 @@ data_ch['Date'] = pd.to_datetime(data_ch['Date'])
 # Membuat dictionary dari dataset
 country_province_dict = data_ch.groupby('country')['province'].unique().apply(list).to_dict()
 
-st.header("The Trend of COVID-19 Cases from 2020 to 2022", divider="grey")
+st.header("The World Trend of COVID-19 Cases from 2020 to 2022", divider="grey")
 
 # semua widget disini
 with st.expander("Click to Change the Filter Settings"):
@@ -26,7 +28,7 @@ with st.expander("Click to Change the Filter Settings"):
         country_option = st.selectbox(
             "Countries Option",
             list(country_province_dict.keys()),
-            index=0,
+            index=1,
             placeholder="Please select one of the country ...",
         )
     with col2:
@@ -159,10 +161,28 @@ net_op = {
     'Sint Maarten': {'lat': 18.0, 'lon': -63.05},
 }
 
+net_p = {
+    'Aruba': [12.5, -69.97],
+    'Bonaire, Sint Eustatius and Saba': [17.6, -63.06],
+    'Curacao': [12.2, -68.93],
+    'Sint Maarten': [18.0, -63.05],
+}
 
 with st.container():
-    oncol1, oncol2 = st.columns((1,3))
+    oncol1, oncol2, oncol3 = st.columns((1,3,1))
     with oncol1:
+
+        st.write(
+            """
+            <style>
+            [data-testid="stMetricDelta"] svg {
+                display: none;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
+
         confirmed = filter_timeline.Confirmed.sum()
         recovered = int(filter_timeline.Recovered.sum())
         deaths    = filter_timeline.Deaths.sum()
@@ -182,163 +202,208 @@ with st.container():
         st.markdown("####")
         st.metric(label="Total Deaths",    value=f"{deaths:,}",    delta=percen_deaths)
         st.markdown("####")
+
     with oncol2:
         st.markdown("####")
+        with st.container():
+            if (country_option == "Australia"):
+                geojson_data = json.load(open('geojson/australia.geojson'))
+                # Highlight selected country
+                m = folium.Map(location=[-25.2744, 133.7751], zoom_start=4)  # Adjust the starting location and zoom level as needed
 
-        if (country_option == "Australia"):
-            geojson_data = json.load(open('geojson/australia.geojson'))
-            # Highlight selected country
-            m = folium.Map(location=[-25.2744, 133.7751], zoom_start=4)  # Adjust the starting location and zoom level as needed
-
-            folium.GeoJson(
-                geojson_data,
-                name="geojson",
-                style_function=style_function,
-                tooltip=folium.GeoJsonTooltip(fields=['STATE_NAME'], aliases=['Province/State:'])
-            ).add_to(m)
-
-            folium.LayerControl().add_to(m)
-            folium_static(m, width=950, height=500)
-
-        elif(country_option == "United Kingdom"):
-            geojson_data = json.load(open('geojson/united_kingdom.geojson'))
-            m = folium.Map(location=uk_op[province_option], zoom_start=8)  # Adjust the starting location and zoom level as needed
-
-            for feature in geojson_data['features']:
-                region_name = feature['properties']['name']
-            
-            folium.LayerControl().add_to(m)
-
-            for location, coord in uk_op.items():
-                add_marker(m, uk_op[province_option][0],  uk_op[province_option][1], uk_keys.query(f"name=='{province_option}'").name.to_string())
-
-            folium_static(m, width=950, height=500)
-        elif(country_option == "New Zealand"):
-            geojson_data = json.load(open('geojson/nz.geojson'))
-            m = folium.Map(location=nz_op[province_option], zoom_start=4)  # Adjust the starting location and zoom level as needed
-
-            for feature in geojson_data['features']:
-                region_name = feature['properties']['name']
-            
-            folium.LayerControl().add_to(m)
-
-            for location, coord in nz_op.items():
-                add_marker(m, nz_op[province_option][0],  nz_op[province_option][1], "Cook Island")
-
-            folium_static(m, width=950, height=500)
-        elif(country_option == "France"):
-            # Membaca file GeoJSON France
-            with open('geojson/france.geojson', 'r') as f:
-                geojson_data = json.load(f)
-
-            # Membuat objek peta
-            m = folium.Map(location=fr_p[province_option], zoom_start=4)
-
-            # Menambahkan layer GeoJSON ke peta tanpa gaya yang berbeda untuk setiap region
-            for feature in geojson_data['features']:
-                province_name = feature['properties']['name']
                 folium.GeoJson(
-                    feature,
-                    name=province_name,
-                    tooltip=province_name
+                    geojson_data,
+                    name="geojson",
+                    style_function=style_function,
+                    tooltip=folium.GeoJsonTooltip(fields=['STATE_NAME'], aliases=['Province/State:'])
                 ).add_to(m)
 
-            # Menambahkan marker atau label untuk setiap daerah yang akan ditandai
-            for location, coord in fr_op.items():
-                add_marker(m, coord['lat'], coord['lon'], location)
-
-            # Menambahkan kontrol layer
-            folium.LayerControl().add_to(m)
-
-            # Menampilkan peta di notebook
-            folium_static(m, width=950, height=500)
-
-        elif (country_option == "China"):
-            # Load geospatial data for Chinese provinces
-            china_map = gpd.read_file('geojson/china geojson/chn_admbnda_adm1_ocha_2020.shp')
-
-            # Function to create interactive map
-            def create_map():
-                # Create the Folium map
-                m = folium.Map(location=[35, 105], zoom_start=4, tiles='cartodb positron')
-
-                # Add GeoJSON layer for China provinces
-                folium.GeoJson(
-                    china_map,
-                    name='China Province COVID-19 Data',
-                    style_function=lambda feature: {
-                        'fillColor': 'yellow' if feature['properties']['ADM1_EN'] == province_option else '#3186cc',
-                        'color': 'black',
-                        'weight': 1,
-                        'fillOpacity': 0.6
-                    },
-                    tooltip=folium.features.GeoJsonTooltip(
-                        fields=['ADM1_EN'],
-                        aliases=['Province:'],
-                        labels=True,
-                        localize=True,
-                        style=('background-color: white; color: #333333; font-family: sans-serif')
-                    )
-                ).add_to(m)
                 folium.LayerControl().add_to(m)
-                folium_static(m, width=950, height=500)
+                folium_static(m, width=730, height=500)
 
-            # Call the function to create and display the map
-            create_map()
+            elif(country_option == "United Kingdom"):
+                geojson_data = json.load(open('geojson/united_kingdom.geojson'))
+                m = folium.Map(location=uk_op[province_option], zoom_start=8)  # Adjust the starting location and zoom level as needed
 
-        elif(country_option == "Denmark"):
-            # Membaca file GeoJSON Denmark
-            with open('geojson/denmark.geojson', 'r') as f:
-                denmark_geojson = json.load(f)
+                for feature in geojson_data['features']:
+                    region_name = feature['properties']['name']
+                
+                folium.LayerControl().add_to(m)
 
-            # Membuat objek peta
-            m = folium.Map(location=[68.0, 0.0], zoom_start=4)  # Koordinat tengah untuk Denmark
+                for location, coord in uk_op.items():
+                    add_marker(m, uk_op[province_option][0],  uk_op[province_option][1], uk_keys.query(f"name=='{province_option}'").name.to_string())
 
-            # Menambahkan layer GeoJSON ke peta tanpa gaya yang berbeda untuk setiap region
-            for feature in denmark_geojson['features']:
-                province_name = feature['properties']['name']
-                folium.GeoJson(
-                    feature,
-                    name=province_name,
-                    tooltip=province_name
-                ).add_to(m)
+                folium_static(m, width=730, height=500)
 
-            # Menambahkan marker atau label untuk setiap daerah yang akan ditandai
-            for location, coord in dn_op.items():
-                add_marker(m, coord['lat'], coord['lon'], location)
+            elif(country_option == "New Zealand"):
+                geojson_data = json.load(open('geojson/nz.geojson'))
+                m = folium.Map(location=nz_op[province_option], zoom_start=4)  # Adjust the starting location and zoom level as needed
 
-            # Menambahkan kontrol layer
-            folium.LayerControl().add_to(m)
+                for feature in geojson_data['features']:
+                    region_name = feature['properties']['name']
+                
+                folium.LayerControl().add_to(m)
 
-            # Menampilkan peta di notebook
-            folium_static(m, width=950, height=500)
+                for location, coord in nz_op.items():
+                    add_marker(m, nz_op[province_option][0],  nz_op[province_option][1], "Cook Island")
 
-        elif(country_option == "Netherlands"):
-            # Membaca file GeoJSON Netherlands
-            with open('geojson/netherlands.geojson', 'r') as f:
-                netherlands_geojson = json.load(f)
+                folium_static(m, width=730, height=500)
 
-            # Membuat objek peta
-            m = folium.Map(location=[12.5, -69.97], zoom_start=8)  # Koordinat tengah untuk Aruba
+            elif(country_option == "France"):
+                # Membaca file GeoJSON France
+                with open('geojson/france.geojson', 'r') as f:
+                    geojson_data = json.load(f)
 
-            # Menambahkan layer GeoJSON ke peta tanpa gaya yang berbeda untuk setiap region
-            for feature in netherlands_geojson['features']:
-                province_name = feature['properties']['name']
-                folium.GeoJson(
-                    feature,
-                    name=province_name,
-                    tooltip=province_name
-                ).add_to(m)
+                # Membuat objek peta
+                m = folium.Map(location=fr_p[province_option], zoom_start=4)
 
-            # Menambahkan marker atau label untuk setiap daerah yang akan ditandai
-            for location, coord in net_op.items():
-                add_marker(m, coord['lat'], coord['lon'], location)
+                # Menambahkan layer GeoJSON ke peta tanpa gaya yang berbeda untuk setiap region
+                for feature in geojson_data['features']:
+                    province_name = feature['properties']['name']
+                    folium.GeoJson(
+                        feature,
+                        name=province_name,
+                        tooltip=province_name
+                    ).add_to(m)
 
-            # Menambahkan kontrol layer
-            folium.LayerControl().add_to(m)
+                # Menambahkan marker atau label untuk setiap daerah yang akan ditandai
+                for location, coord in fr_op.items():
+                    add_marker(m, coord['lat'], coord['lon'], location)
 
-            # Menampilkan peta di notebook
-            folium_static(m, width=950, height=500)
+                # Menambahkan kontrol layer
+                folium.LayerControl().add_to(m)
+
+                # Menampilkan peta di notebook
+                folium_static(m, width=730, height=500)
+
+            elif (country_option == "China"):
+                # Load geospatial data for Chinese provinces
+                china_map = gpd.read_file('geojson/china geojson/chn_admbnda_adm1_ocha_2020.shp')
+
+                # Function to create interactive map
+                def create_map():
+                    # Create the Folium map
+                    m = folium.Map(location=[35, 105], zoom_start=4, tiles='cartodb positron')
+
+                    # Add GeoJSON layer for China provinces
+                    folium.GeoJson(
+                        china_map,
+                        name='China Province COVID-19 Data',
+                        style_function=lambda feature: {
+                            'fillColor': 'yellow' if feature['properties']['ADM1_EN'] == province_option else '#3186cc',
+                            'color': 'black',
+                            'weight': 1,
+                            'fillOpacity': 0.6
+                        },
+                        tooltip=folium.features.GeoJsonTooltip(
+                            fields=['ADM1_EN'],
+                            aliases=['Province:'],
+                            labels=True,
+                            localize=True,
+                            style=('background-color: white; color: #333333; font-family: sans-serif')
+                        )
+                    ).add_to(m)
+                    folium.LayerControl().add_to(m)
+                    folium_static(m, width=730, height=500)
+
+                # Call the function to create and display the map
+                create_map()
+
+            elif(country_option == "Denmark"):
+                # Membaca file GeoJSON Denmark
+                with open('geojson/denmark.geojson', 'r') as f:
+                    denmark_geojson = json.load(f)
+
+                # Membuat objek peta
+                m = folium.Map(location=[68.0, 0.0], zoom_start=4)  # Koordinat tengah untuk Denmark
+
+                # Menambahkan layer GeoJSON ke peta tanpa gaya yang berbeda untuk setiap region
+                for feature in denmark_geojson['features']:
+                    province_name = feature['properties']['name']
+                    folium.GeoJson(
+                        feature,
+                        name=province_name,
+                        tooltip=province_name
+                    ).add_to(m)
+
+                # Menambahkan marker atau label untuk setiap daerah yang akan ditandai
+                for location, coord in dn_op.items():
+                    add_marker(m, coord['lat'], coord['lon'], location)
+
+                # Menambahkan kontrol layer
+                folium.LayerControl().add_to(m)
+
+                # Menampilkan peta di notebook
+                folium_static(m, width=730, height=500)
+
+            elif(country_option == "Netherlands"):
+                # Membaca file GeoJSON Netherlands
+                with open('geojson/netherlands.geojson', 'r') as f:
+                    netherlands_geojson = json.load(f)
+
+                # Membuat objek peta
+                m = folium.Map(location=net_p[province_option], zoom_start=8)  # Koordinat tengah untuk Aruba
+
+                # Menambahkan layer GeoJSON ke peta tanpa gaya yang berbeda untuk setiap region
+                for feature in netherlands_geojson['features']:
+                    province_name = feature['properties']['name']
+                    folium.GeoJson(
+                        feature,
+                        name=province_name,
+                        tooltip=province_name
+                    ).add_to(m)
+
+                # Menambahkan marker atau label untuk setiap daerah yang akan ditandai
+                for location, coord in net_op.items():
+                    add_marker(m, coord['lat'], coord['lon'], location)
+
+                # Menambahkan kontrol layer
+                folium.LayerControl().add_to(m)
+
+                # Menampilkan peta di notebook
+                folium_static(m, width=730, height=500)
+
+    with oncol3:
+        cfr = (deaths / confirmed) * 100
+        recovery_rate = (recovered / confirmed) * 100
+        active_cases_rate = ((confirmed - recovered - deaths) / confirmed) * 100
+
+        fig = go.Figure()
+        with st.container():
+            fig.add_trace(go.Indicator(
+                mode="gauge+number",
+                value=cfr,
+                title={'text': "Case Fatality Rate", 'font': {'size': 12}},
+                domain={'x': [0, 1], 'y': [0.8, 1]},
+                gauge={'axis': {'range': [None, 100]},
+                    'bar': {'color': "#f10096"}},
+            ))
+        
+        with st.container():
+            fig.add_trace(go.Indicator(
+                mode="gauge+number",
+                value=recovery_rate,
+                title={'text': "Recovery Rate", 'font': {'size': 12}},
+                domain={'x': [0, 1], 'y': [0.4, 0.6]},
+                gauge={'axis': {'range': [None, 100]},
+                    'bar': {'color': "#00b6cb"}},
+            ))
+
+        with st.container():
+            fig.add_trace(go.Indicator(
+                mode="gauge+number",
+                value=active_cases_rate,
+                title={'text': "Active Cases Rate", 'font': {'size': 12}},
+                domain={'x': [0, 1], 'y': [0, 0.20]},
+                gauge={'axis': {'range': [None, 100]},
+                    'bar': {'color': "#0072f0"}},
+            ))
+
+        fig.update_layout(
+                        template="plotly_white",  # Choose your template
+                        width=400, height=600,
+                        )
+        st.plotly_chart(fig)
 
 with st.container():
     # visualisasi linechart
@@ -349,6 +414,7 @@ with st.container():
                         "Recovered": "#00b6cb",
                         "Deaths": "#f10096"
                     },
+                    labels={'value':'Number of People'},
                     template="plotly_white", height=500)
 
     fig.update_layout(legend=dict(
